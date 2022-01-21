a63466e08fd2731306dffadd317f6bdae05efbcf
Artem Pavlov <pavlov.tema@gmail.com>
2022-01-22 00:10:55 +0500

    updated gradient on LoginViewController.

--- My skills/LoginViewController.swift
+++ My skills/LoginViewController.swift
@@ -11,16 +11,16 @@ class LoginViewController: UIViewController {
     
     //первый цвет для градиента
     private let topColor = UIColor(
-        red: 210/255,
-        green: 109/255,
-        blue: 128/255,
+        red: 215/255,
+        green: 210/255,
+        blue: 230/255,
         alpha: 1
     )
     //второй цвет для градиента
     private let bottomColor = UIColor(
-        red: 107/255,
-        green: 148/255,
-        blue: 220/255,
+        red: 180/255,
+        green: 180/255,
+        blue: 210/255,
         alpha: 1
     )
     
@@ -30,8 +30,9 @@ class LoginViewController: UIViewController {
     }
 }
 
-//устанавливаем градиент для фона
+
 //MARK: - Set background color
+//устанавливаем градиент для фона
 extension UIView {
     func addVerticalGradientLayer(topColor: UIColor, bottomColor: UIColor) {
         let gradient = CAGradientLayer()
