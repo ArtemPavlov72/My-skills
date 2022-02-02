789845b763c605bd316049eaddba396215160203
Artem Pavlov <pavlov.tema@gmail.com>
2022-02-02 17:51:42 +0500

    Model Project must be simple.

--- My skills/Models/Project.swift
+++ My skills/Models/Project.swift
@@ -5,40 +5,7 @@
 //  Created by iMac on 29.01.2022.
 //
 
-struct Project {
-    let title: String
-    let description: String
-    
-    var fullDescription: String {
-        "\(title). \(description)"
-    }
-}
-
-extension Project {
-    static func getProjectsList() -> [Project] {
-        var projects: [Project] = []
-        
-        for projectCase in ProjectDescription.allCases {
-            let project = Project(
-                title: projectCase.projectData.title,
-                description: projectCase.projectData.description
-            )
-            projects.append(project)
-        }
-        return projects
-    }
-}
-
-enum ProjectDescription: CaseIterable {
-    case trafficLights,
-         quiz
-    
-    var projectData: Project {
-        switch self {
-        case .trafficLights:
-            return Project(title: "Светофор", description: "Переключаем цвета на светофоре")
-        case .quiz:
-            return Project(title: "Квиз", description: "Узнай, какое ты животное!")
-        }
-    }
+enum Project: String, CaseIterable {
+    case trafficLights = "Светофор",
+         quiz = "Квиз"
 }
