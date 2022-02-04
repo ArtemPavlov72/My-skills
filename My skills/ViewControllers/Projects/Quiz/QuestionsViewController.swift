d26a69612bc852833e3593c01e596f6e970019bb
Artem Pavlov <pavlov.tema@gmail.com>
2022-02-04 18:42:43 +0500

    QuestionsViewController logic updated.

--- My skills/ViewControllers/Projects/Quiz/QuestionsViewController.swift
+++ My skills/ViewControllers/Projects/Quiz/QuestionsViewController.swift
@@ -8,12 +8,123 @@
 import UIKit
 
 class QuestionsViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-       
+    
+    // MARK: - IB Outlets
+    @IBOutlet var questionProgressView: UIProgressView!
+    @IBOutlet var questionLabel: UILabel!
+    
+    @IBOutlet var singleAnswersStackView: UIStackView!
+    @IBOutlet var singleButtons: [UIButton]!
+    
+    @IBOutlet var multipleAnswersStackView: UIStackView!
+    @IBOutlet var multipleLabels: [UILabel]!
+    @IBOutlet var multipleSwitches: [UISwitch]!
+    
+    @IBOutlet var rangedAnswersStackView: UIStackView!
+    @IBOutlet var rangedLabels: [UILabel]!
+    @IBOutlet var rangedSlider: UISlider! {
+        didSet{
+            let answersCount = Float(currentAnswers.count - 1)
+            rangedSlider.maximumValue = answersCount
+            rangedSlider.value = answersCount / 2
+        }
     }
     
+    // MARK: - Private Properties
+    private let questions = Question.getQuestions()
+    private var questionIndex = 0
+    private var currentAnswers: [Answer] {
+        questions[questionIndex].answers
+    }
+    private var answersChosen: [Answer] = []
+    
+    // MARK: - Life Cycles Methods
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateUI()
+        
+    }
+    
+    @IBAction func singleButtonPressed(_ sender: UIButton) {
+        guard let buttonIndex = singleButtons.firstIndex(of: sender) else {return}
+        let currentAnswer = currentAnswers[buttonIndex]
+        answersChosen.append(currentAnswer)
+        nextQuestion()
+    }
+    
+    @IBAction func multipleButtonPressed() {
+        for (multipleSwitch, answer) in zip(multipleSwitches, currentAnswers) {
+            if multipleSwitch.isOn {
+                answersChosen.append(answer)
+            }
+        }
+        nextQuestion()
+    }
+    
+    @IBAction func rangedButtonPressed() {
+        let index = lrintf(rangedSlider.value)
+        answersChosen.append(currentAnswers[index])
+        nextQuestion()
+    }
+}
 
+// MARK: - Private Methods
+extension QuestionsViewController {
+    private func updateUI() {
+        for stackView in [singleAnswersStackView, multipleAnswersStackView, rangedAnswersStackView] {
+            stackView?.isHidden = true
+        }
+        
+        let currentQuestion = questions[questionIndex]
+        questionLabel.text = currentQuestion.title
+        
+        let answersProgress = Float(questionIndex) / Float(questions.count)
+        questionProgressView.setProgress(answersProgress, animated: true)
+        
+        title = "Вопрос № \(questionIndex + 1) из \(questions.count)"
+        
+        showCurrentAnswers(for: currentQuestion.type)
+    }
+    
+    private func showCurrentAnswers(for type: ResponseType) {
+        switch type {
+        case .singe: showSingleStackView(with: currentAnswers)
+        case .multiple: showMultipleStackView(with: currentAnswers)
+        case .ranged: showRangedStackView(with: currentAnswers)
+        }
+    }
+    
+    private func showSingleStackView(with answers: [Answer]) {
+        singleAnswersStackView.isHidden.toggle()
+        
+        for (button, answer) in zip(singleButtons, answers) {
+            button.setTitle(answer.title, for: .normal)
+        }
+    }
+    
+    private func showMultipleStackView(with answers: [Answer]) {
+        multipleAnswersStackView.isHidden.toggle()
+        
+        for (label, answer) in zip(multipleLabels, answers) {
+            label.text = answer.title
+        }
+    }
+    
+    private func showRangedStackView(with answers: [Answer]) {
+        rangedAnswersStackView.isHidden.toggle()
+        
+        rangedLabels.first?.text = answers.first?.title
+        rangedLabels.last?.text = answers.last?.title
+    }
+    
+    private func nextQuestion() {
+        questionIndex += 1
+        
+        if questionIndex < questions.count {
+            updateUI()
+            return
+        }
+        performSegue(withIdentifier: "showResult", sender: nil)
+    }
+    
 }
