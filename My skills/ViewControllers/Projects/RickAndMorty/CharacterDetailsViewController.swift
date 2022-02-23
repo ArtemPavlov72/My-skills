4fadde6e24a0ed3e77b9ac83cf3d9741f4dab639
Artem Pavlov <pavlov.tema@gmail.com>
2022-02-23 11:32:49 +0500

    new Label for hero name (Rick and Morty project) NavigationBar Title deleted.

--- My skills/ViewControllers/Projects/RickAndMorty/CharacterDetailsViewController.swift
+++ My skills/ViewControllers/Projects/RickAndMorty/CharacterDetailsViewController.swift
@@ -10,13 +10,14 @@ import UIKit
 class CharacterDetailsViewController: UIViewController {
     
     //MARK: - IB Outlets
-    @IBOutlet var characterImage: UIImageView! {
+    @IBOutlet var heroImage: UIImageView! {
         didSet {
-            characterImage.layer.cornerRadius = characterImage.frame.height / 2
+            heroImage.layer.cornerRadius = heroImage.frame.height / 2
         }
     }
     
-    @IBOutlet var characterDetailsLabel: UILabel!
+    @IBOutlet var heroNameLabel: UILabel!
+    @IBOutlet var heroDetailsLabel: UILabel!
     
     //MARK: - Public Properties
     var hero: Hero!
@@ -25,9 +26,9 @@ class CharacterDetailsViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationItem.largeTitleDisplayMode = .never
-        setTitle()
         
-        characterDetailsLabel.text = hero.description
+        heroNameLabel.text = hero.name
+        heroDetailsLabel.text = hero.description
         loadImage(from: hero.image)
     }
     
@@ -38,17 +39,8 @@ class CharacterDetailsViewController: UIViewController {
             guard let imageData = try? Data(contentsOf: urlImage) else {return}
             
             DispatchQueue.main.async {
-                self.characterImage.image = UIImage(data: imageData)
+                self.heroImage.image = UIImage(data: imageData)
             }
         }
-    }
-    
-    private func setTitle() {
-        let titleLabel = UILabel()
-        titleLabel.text = "\(hero.name)"
-        titleLabel.font = UIFont.systemFont(ofSize: 22, weight: UIFont.Weight.bold)
-        titleLabel.adjustsFontSizeToFitWidth = true
-        titleLabel.minimumScaleFactor = 0.5
-        navigationItem.titleView = titleLabel
     }
 }
