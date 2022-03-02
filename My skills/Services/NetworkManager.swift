579a0b3e4cbbc4f1aad0f3f8417eae3fb92eaf51
Artem Pavlov <pavlov.tema@gmail.com>
2022-03-02 23:26:53 +0500

    NetworkManager created.

---
+++ My skills/Services/NetworkManager.swift
@@ -0,0 +1,45 @@
+//
+//  NetworkManager.swift
+//  My skills
+//
+//  Created by iMac on 24.02.2022.
+//
+
+import Foundation
+
+enum NetworkError: Error {
+    case invalidURL
+    case noData
+    case decodingError
+}
+
+class NetworkManager {
+    
+    static let shared = NetworkManager()
+    
+    private init() {}
+    
+    func fetchData(from url: String, completion: @escaping(Result<RickAndMorty, NetworkError>) -> Void) {
+        guard let url = URL(string: url) else {
+            completion(.failure(.invalidURL))
+            return
+        }
+        
+        URLSession.shared.dataTask(with: url) { data, _, error in
+            guard let data = data else {
+                completion(.failure(.noData))
+                print(error?.localizedDescription ?? "no description")
+                return
+            }
+            
+            do {
+                let rickAndMorty = try JSONDecoder().decode(RickAndMorty.self, from: data)
+                DispatchQueue.main.async {
+                    completion(.success(rickAndMorty))
+                }
+            } catch {
+                completion(.failure(.decodingError))
+            }
+        } .resume()
+    }
+}
