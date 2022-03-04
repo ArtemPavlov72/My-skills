5002e9007fe7dfb22a3fe9c5b78384f2232a68ad
Artem Pavlov <pavlov.tema@gmail.com>
2022-03-04 23:38:53 +0500

    Add activityIndicator for image (RickAndMortyCell)

--- My skills/ViewControllers/Projects/RickAndMorty/RickAndMortyCell.swift
+++ My skills/ViewControllers/Projects/RickAndMorty/RickAndMortyCell.swift
@@ -17,15 +17,33 @@ class RickAndMortyCell: UITableViewCell {
     }
     @IBOutlet weak var heroNameLabel: UILabel!
     
+    //MARK: - Private Outlets
+    private var spinnerView: UIActivityIndicatorView?
+    
     //MARK: - Methods
     func configure(with hero: Hero?) {
         heroNameLabel.text = hero?.name
+        
+        spinnerView = showSpinner(in: heroImage)
         
         DispatchQueue.global().async {
             guard let imageData = ImageManager.shared.loadImage(from: hero?.image) else {return}
             DispatchQueue.main.async {
                 self.heroImage.image = UIImage(data: imageData)
+                self.spinnerView?.stopAnimating()
             }
         }
+    }
+    
+    //MARK: - Private Methods
+    private func showSpinner(in view: UIView) -> UIActivityIndicatorView {
+        let activityIndicator = UIActivityIndicatorView(style: .medium)
+        activityIndicator.startAnimating()
+        activityIndicator.center = view.center
+        activityIndicator.hidesWhenStopped = true
+        
+        view.addSubview(activityIndicator)
+        
+        return activityIndicator
     }
 }
