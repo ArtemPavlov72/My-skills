3925da69326b8887d4a79dca48fc57c4075340ed
Artem Pavlov <pavlov.tema@gmail.com>
2022-03-15 18:20:45 +0500

    New method for fetch image in NetworkManager (for cache).

--- My skills/Services/NetworkManager.swift
+++ My skills/Services/NetworkManager.swift
@@ -81,4 +81,17 @@ class ImageManager {
                 }
             }
     }
+    
+    func loadImageWithCache(from url: URL, completion: @escaping(Data, URLResponse) -> Void) {
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            guard let data = data, let response = response else {
+                print(error?.localizedDescription ?? "No error description")
+                return
+            }
+            guard url == response.url else {return}
+            DispatchQueue.main.async {
+                completion(data, response)
+            }
+        }.resume()
+    }
 }
---
+++ My skills/ViewControllers/Projects/RickAndMorty/HeroImageView.swift
@@ -0,0 +1,17 @@
+//
+//  HeroImageView.swift
+//  My skills
+//
+//  Created by iMac on 15.03.2022.
+//
+
+import UIKit
+
+class HeroImageView: UIImageView {
+    
+    
+    
+    
+    
+    
+}
