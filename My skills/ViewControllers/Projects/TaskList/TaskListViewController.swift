2a18b252ec919501d91e2a8a81f6789067f10969
ArtemPavlov72 <pavlov.tema@gmail.com>
2022-03-30 22:48:28 +0500

    TaskList project, new func for deleting table row - using trailingSwipeActionsConfigurationForRowAt.

--- My skills/ViewControllers/Projects/TaskList/TaskListViewController.swift
+++ My skills/ViewControllers/Projects/TaskList/TaskListViewController.swift
@@ -46,7 +46,20 @@ class TaskListViewController: UITableViewController {
     }
     
     //MARK: - Table View Delegate
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+    override func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        tableView.deselectRow(at: indexPath, animated: true)
+        let task = tasks[indexPath.row]
+        
+        let deleteAction = UIContextualAction(style: .destructive, title: "Delete") { _, _, _ in
+            self.tasks.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+            StorageManager.shared.deleteData(task)
+        }
+        return UISwipeActionsConfiguration(actions: [deleteAction])
+    }
+    
+    
+   /* override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         let task = tasks[indexPath.row]
         
         if editingStyle == .delete {
@@ -55,14 +68,14 @@ class TaskListViewController: UITableViewController {
             StorageManager.shared.deleteData(task)
         }
     }
-    
+     */
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
         let task = tasks[indexPath.row]
         showAlert(task: task) {
             tableView.reloadRows(at: [indexPath], with: .automatic)
         }
-    }
+    } 
     
     // MARK: - Private Methods
     private func setupNavigationBar() {
