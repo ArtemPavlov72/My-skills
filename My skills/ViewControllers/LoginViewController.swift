640b746c05ed6a7433671c08c13b32c7f582e45c
ArtemPavlov72 <pavlov.tema@gmail.com>
2022-08-07 23:52:22 +0500

    Application language changed to English

--- My skills/ViewControllers/LoginViewController.swift
+++ My skills/ViewControllers/LoginViewController.swift
@@ -84,7 +84,7 @@ extension UIView {
         gradient.frame = bounds
         gradient.colors = [topColor.cgColor, bottomColor.cgColor]
         gradient.locations = [0.0, 1.0]
-        gradient.startPoint = CGPoint(x: 0, y: 0)
+        gradient.startPoint = CGPoint(x: 0, y: 0) 
         gradient.endPoint = CGPoint(x: 0, y: 1)
         layer.insertSublayer(gradient, at: 0)
     }
